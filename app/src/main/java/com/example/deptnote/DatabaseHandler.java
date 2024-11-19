dcbe947512547cf8d230d88d7c6468b6ac0ae5d1
anhhai2909 <132214086+anhhai2909@users.noreply.github.com>
2024-11-19 21:15:11 +0700

    .

---
+++ app/src/main/java/com/example/deptnote/DatabaseHandler.java
@@ -0,0 +1,19 @@
+package com.example.deptnote;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+public class DatabaseHandler extends SQLiteOpenHelper{
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
